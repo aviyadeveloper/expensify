2f346ac236b006c40a0439bcaf51778672c251a6
Aviya Developer <aviya.developer@gmail.com>
2019-05-16 12:56:12 +0300

    Script naming changed and fixed bug with dashboard page component

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { RouteProps } from "react-router";
-import ExpenseList from "./ExpenseList";
+import React from 'react';
+import { RouteProps } from 'react-router';
+import ExpenseList from './ExpenseList';
+import { filtersReducerDefaultState } from '../reducers/filtersReducer';
 
 export const DashboardPage: React.SFC<RouteProps> = props => (
   <div>
-    <ExpenseList />
+    <ExpenseList filters={filtersReducerDefaultState} />
   </div>
 );
--- src/reducers/filtersReducer.ts
+++ src/reducers/filtersReducer.ts
@@ -1,11 +1,11 @@
-import { FiltersReducerState, FiltersSortBy } from "../types/filtersTypes";
-import moment = require("moment");
+import { FiltersReducerState, FiltersSortBy } from '../types/filtersTypes';
+import moment = require('moment');
 
-const filtersReducerDefaultState: FiltersReducerState = {
-  name: "",
+export const filtersReducerDefaultState: FiltersReducerState = {
+  name: '',
   sortBy: FiltersSortBy.Date,
-  startDate: moment().startOf("month"),
-  endDate: moment().endOf("month")
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 
 export default (
@@ -13,15 +13,15 @@ export default (
   action: any
 ) => {
   switch (action.type) {
-    case "SET_NAME_FILTER":
+    case 'SET_NAME_FILTER':
       return { ...state, name: action.name };
-    case "SORT_BY_DATE":
+    case 'SORT_BY_DATE':
       return { ...state, sortBy: FiltersSortBy.Date };
-    case "SORT_BY_AMOUNT":
+    case 'SORT_BY_AMOUNT':
       return { ...state, sortBy: FiltersSortBy.Amount };
-    case "SET_START_DATE":
+    case 'SET_START_DATE':
       return { ...state, startDate: action.date };
-    case "SET_END_DATE":
+    case 'SET_END_DATE':
       return { ...state, endDate: action.date };
     default:
       return state;
