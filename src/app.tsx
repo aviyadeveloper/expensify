e0cee4f232f1cf1321e3b54c3ab9683df33b5c40
Aviya Developer <aviya.developer@gmail.com>
2019-05-19 17:51:35 +0300

    Fixed import moment call

--- src/app.tsx
+++ src/app.tsx
@@ -1,41 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { AppRouter } from "./routers/AppRouter";
-import configureStore from "./store/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { AppRouter } from './routers/AppRouter';
+import configureStore from './store/configureStore';
 
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
 
-import { addExpense } from "./actions/expensesActions";
-import { sortByAmount } from "./actions/filtersActions";
-import moment = require("moment");
+import { addExpense } from './actions/expensesActions';
+import { sortByAmount } from './actions/filtersActions';
+import moment from 'moment';
 
 const store = configureStore();
 store.dispatch(
   addExpense({
-    name: "Water Bill",
+    name: 'Water Bill',
     amount: 1000,
-    description: "June 2010",
+    description: 'June 2010',
     createdAt: moment()
   })
 );
 store.dispatch(
   addExpense({
-    name: "Gas Bill",
+    name: 'Gas Bill',
     amount: 2000,
-    description: "June 2010",
+    description: 'June 2010',
     createdAt: moment()
   })
 );
 
 store.dispatch(
   addExpense({
-    name: "Ice Cream",
+    name: 'Ice Cream',
     amount: 200,
-    description: "Tasty",
+    description: 'Tasty',
     createdAt: moment()
   })
 );
@@ -47,4 +47,4 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
-ReactDOM.render(jsx, document.getElementById("myApp"));
+ReactDOM.render(jsx, document.getElementById('myApp'));
