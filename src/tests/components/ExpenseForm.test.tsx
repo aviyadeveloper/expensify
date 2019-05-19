c99641f95135726a17a11a3a9192f0b57a4a43e3
Aviya Developer <aviya.developer@gmail.com>
2019-05-19 18:25:27 +0300

    Added ExpenseForm tests for inputs name, amount and description

--- src/tests/components/ExpenseForm.test.tsx
+++ src/tests/components/ExpenseForm.test.tsx
@@ -12,3 +12,52 @@ test('should render ExpenseForm with expense data', () => {
   const wrapper = shallow(<ExpenseForm {...expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should change state name if input value changes', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  const input = wrapper.find('input#expense-form-name');
+  input.simulate('change', {
+    currentTarget: {
+      value: 'foobar'
+    }
+  });
+  expect(wrapper.state('name')).toBe('foobar');
+});
+
+test('should change state amount if input value changes and valid', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  const input = wrapper.find('input#expense-form-amount');
+  input.simulate('change', {
+    currentTarget: {
+      value: '100'
+    }
+  });
+  expect(wrapper.state('amount')).toBe('100');
+});
+
+test('should not change state amount if input value changes and invalid', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  const input = wrapper.find('input#expense-form-amount');
+  input.simulate('change', {
+    currentTarget: {
+      value: '100.1234'
+    }
+  });
+  expect(wrapper.state('amount')).toBe('');
+});
+
+//put date test here
+// test('should change state createdAt if input value changes', () => {
+//   const wrapper = shallow(<ExpenseForm />);
+// });
+
+test('should change state description if input value changes', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  const textarea = wrapper.find('textarea#expense-form-description');
+  textarea.simulate('change', {
+    currentTarget: {
+      value: 'description here'
+    }
+  });
+  expect(wrapper.state('description')).toBe('description here');
+});
